178c761a4b153313a1834b0108610e3fd2185d7b
Taís Oliveira <tsoliveira868@gmail.com>
2023-03-01 03:45:08 -0300

    criando os componentes e páginas principais do projeto

---
+++ src/pages/cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Cart.module.scss'
+
+const Cart = () => {
+    return (
+        <div>
+            <h1>Cart</h1>
+        </div>
+    )
+}
+
+export default Cart
---
+++ src/pages/myOrders/MyOrders.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './MyOrders.module.scss'
+
+const MyOrders = () => {
+    return (
+        <div>
+            <h1>My Orders</h1>
+        </div>
+    )
+}
+
+export default MyOrders
